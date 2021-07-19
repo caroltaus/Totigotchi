260f47d7fe05ca6e610d02e1538237c7d0e4e9d4
caroltaus <carolinetaus@hotmail.com>
2021-07-19 13:32:32 -0300

    Initial Commit

---
+++ Totigotchi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Totigotchi
+//
+//  Created by Caroline Taus on 19/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
